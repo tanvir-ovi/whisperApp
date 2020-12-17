63dbed13078ff15ed129861f85f8dea6d2e4312d
tanvir-ovi <tanvirovi6@gmail.com>
2020-12-17 13:04:00 +0600

    environment variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -20,9 +21,8 @@ const userSchema = new mongoose.Schema ({
 	password: String
 });
 
-const secret = "Thisisourlittlesecret.";
 userSchema.plugin(encrypt, { 
-	secret: secret , 
+	secret: process.env.SECRET , 
 	encryptedFields: ['password'] 
 });
 
